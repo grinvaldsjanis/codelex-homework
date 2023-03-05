d9f95340eea5c070cf2a947b3448344ded30aa17
grinvaldsjanis <grinvalds.janis@gmail.com>
2023-03-05 20:14:38 +0200

    minor updates

--- md24_animal-list/animal-list-angular/src/app/animal-form/animal-form.component.ts
+++ md24_animal-list/animal-list-angular/src/app/animal-form/animal-form.component.ts
@@ -30,38 +30,3 @@ export class AnimalFormComponent {
     }
   }
 }
-
-// import { Component, EventEmitter, Output } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Animal } from '../../utils/animal.interface';
-// import { AnimalService } from '../animal-service.service';
-
-// @Component({
-//   selector: 'app-animal-form',
-//   templateUrl: './animal-form.component.html',
-//   styleUrls: ['./animal-form.component.scss'],
-// })
-// export class AnimalFormComponent {
-//   animalForm: FormGroup;
-//   @Output() animalAdded = new EventEmitter<Animal>();
-//   classOptions = ['mammal', 'bird', 'reptile', 'fish', 'insect', 'amphibian'];
-
-//   constructor(private fb: FormBuilder, private animalService: AnimalService) {
-//     this.animalForm = this.fb.group({
-//       name: ['', Validators.required],
-//       aclass: ['', Validators.required],
-//     });
-//   }
-
-//   onSubmit() {
-//     const animal: Animal = {
-//       name: this.animalForm.value.name,
-//       aclass: this.animalForm.value.aclass,
-//     };
-//     this.animalService.addAnimal(animal).subscribe((newAnimal: Animal) => {
-//       this.animalForm.reset();
-//       console.log('onSubmit form with animal:', animal);
-//       this.animalAdded.emit(newAnimal);
-//     });
-//   }
-// }
